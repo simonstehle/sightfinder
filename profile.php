f27612dad9d808925efbe571c03127d5f7d50c31
Sven <friedel.sven.sf@gmail.com>
2017-03-22 20:54:23 +0100

    changed profile.php checkbox layout to ul adjustments for checkbox grid layout in css

--- profile.php
+++ profile.php
@@ -1,84 +1,77 @@
 <?php include_once ("header.html"); ?>
 
+    <!--userPicPage-->
+    <div class="container">
+        <div class="row" id="row1">
+            <div class="col-sm-12"><img class="insidePic" src="data/defaultprofile.jpg"></div>
+        </div>
 
-<div class="container">
+        <div class = row>
+            <div class="col-sm-6" id="profileData">
+                <span id="loadUsername"></span>
+                <span id="loadSurename"></span>
+            </div>
+        </div>
 
+        <div class = row>
+            <div class="col-sm-6" id="profileData">
+                <span id="loadAge"></span>,
+                <span id="loadGender"></span>
+            </div>
+        </div>
+        <ul class="checkbox-grid">
+            <li>
+                    <input type="checkbox" id="musicAndCultureCB">
+                    <label for="musicAndCultureCB">Music / Culture</label>
+            </li>
+            <li>
+                <input type="checkbox" id="sportsAndleisureActivitiesCB">
+                <label for="sportsAndleisureActivitiesCB">Sports / Leisure</label>
+            </li>
+            <li>
+                <input type="checkbox" id="natureCB">
+                <label for="natureCB">Nature</label>
+            </li>
+            <li>
+                <input type="checkbox" id="foodAndDrinksCB">
+                <label for="foodAndDrinksCB">Food / Drinks</label>
+            </li>
+            <li>
+                <input type="checkbox" id="spaAndRestCB">
+                <label for="spaAndRestCB">Spa / Wellness</label>
 
-
-    <div class="userPicPage"><img class="insidePic" src="data/defaultprofile.jpg"></div>
-
-
-    <div class="left"><span id="loadUsername"></span></div>
-    <div class="right"><span id="loadSurename"></span></div>
-    <div class="left"><span id="loadAge"></span></div>
-    <div class="right"><span id="loadGender"></span></div>
-
-
-    <div class=" btn btn-success btn-lg " id="savePreferences">
-        <span>Save Changes</span>
+            </li>
+            <li>
+                <input type="checkbox" id="artsCB">
+                <label for="artsCB">Art</label>
+            </li>
+            <li>
+                <input type="checkbox" id="historyCB">
+                <label for="historyCB">History</label>
+            </li>
+            <li>
+                <input type="checkbox" id="shoppingCB">
+                <label for="shoppingCB">Shopping</label>
+            </li>
+            <li>
+                <input type="checkbox" id="animalsCB">
+                <label for="animalsCB">Animals</label>
+            </li>
+            <li>
+                <input type="checkbox" id="culinaryCB">
+                <label for="culinaryCB">Culinary</label>
+            </li>
+        </ul>
+        <!--  <div class=" btn btn-success btn-lg " id="savePreferences"> -->
+        <!-- <div class="centerButton">
+             <div class=" btn btn-success btn-lg " id="savePreferences">
+                 <span>Save Changes</span>
+             </div>
+         </div> -->
+        <div class="centerButton">
+            <a href="swipe.php" class="btn btn-success btn-lg active r" role="button">Start</a>
+        </div>
     </div>
-
-
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="musicAndCultureCB"> Music and Culture
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="sportsAndleisureActivitiesCB"> Sports and leisure Activities
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="natureCB"> Nature
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="foodAndDrinksCB"> Food and Drinks
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="spaAndRestCB"> Spa and rest
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="artsCB"> Arts
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="historyCB"> History
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="shoppingCB"> Shopping
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="animalsCB"> Animals
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox" id="culinaryCB"> Culinary
-        </label>
-    </div>
-
-    <div class="centerButton">
-        <a href="swipe.php" class="btn btn-success btn-lg active r" role="button">Start</a>
-    </div>
-
-
-
-
-</div>
 
 
 <?php include_once ("footer.html"); ?>
-
