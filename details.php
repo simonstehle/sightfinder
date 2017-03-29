710c7f0ca441cec7af7b82e4dd38b3e13f7d0641
simonstehle <simonstehle@icloud.com>
2017-03-29 14:13:36 +0200

    Details Page inserted

---
+++ details.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 16.03.17
+ * Time: 20:37
+ */
+
+
+include_once ("header.html");
+?>
+
+<div class="container">
+
+    <table class="table" id="detailspageTable">
+
+    </table>
+
+
+
+</div>
+
+
+
+
+
+
+
+<?php include_once ("footer.html")?>
--- js/main.js
+++ js/main.js
@@ -208,7 +208,7 @@ function loadOneCategorie(numberInArray, tempCoockieArray) {
             if (field["Kategorie"] == recentCategorie.toString()) {
                 //console.log("Hier: " + recentCategorie);
                 //TODO: thumbnail link in hallandSightsData.json
-                tempHTMLCode = "<tr><td> <img src= "+ field["ThumbnailHref"] +" class='thumbnail'</td><td>" + field["Bezeichnung Sehenswuerdigkeit"] + "</td><td><a href='http://www.aventyrslandet.se/' target='_blank'>Homepage</a></td></tr>";
+                tempHTMLCode = "<tr><td> <img src= "+ field["Thumbnail"] +" class='thumbnail'</td><td>" + field["Bezeichnung Sehenswuerdigkeit"] + "</td><td><a href='details.php?halandid="+field["ID_Halland"] +"' >Homepage</a></td></tr>";
 
 
                 $("#resultstable").append(tempHTMLCode);
@@ -216,6 +216,55 @@ function loadOneCategorie(numberInArray, tempCoockieArray) {
         })
 
     });
+}
+
+/**
+ * Helper function to get variable from URL
+ * @param variable
+ * @returns {*}
+ */
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    alert('Query Variable ' + variable + ' not found');
+}
+
+function loadDetailsPage(){
+    var tempHTMLCodeForTable = "";
+    var recentID = getQueryVariable("halandid");
+
+    console.log("Aktuelle ID= "+recentID);
+
+    $.getJSON("../data/hallandSightsData.json", function (data) {
+        $.each(data, function (i, field) {
+
+            //console.log(i);
+            //console.log(field);
+            //console.log(field["Kategorie"]);
+            //console.log(recentCategorie);
+            if (field["ID_Halland"] == recentID.toString()) {
+                tempHTMLCodeForTable = "<tr><td>ID</td><td>"+field["ID_Halland"] +"</td></tr>" +
+                    "<tr><td>Name</td><td>"+field["Bezeichnung Sehenswuerdigkeit"] +"</td></tr>" +
+                    "<tr><td>Category</td><td>"+field["Kategorie"] +"</td></tr>" +
+                    "<tr><td>Type</td><td>"+field["Typ"] +"</td></tr>" +
+                    "<tr><td>Information</td><td>"+field["Merkmal#1(optional)"] +"</td></tr>" +
+                    "<tr><td>Because you liked</td><td>"+field["Interesse#1"] +"</td></tr>" +
+                    "<tr><td>Adress</td><td>"+field["Adresse"] +"</td></tr>" +
+                    "<tr><td>Price</td><td>"+field["Preise"] +"</td></tr>" +
+                    "<tr><td>Short Info</td><td>"+field["Kurzinfo"] +"</td></tr>";
+                $("#detailspageTable").append(tempHTMLCodeForTable);
+            }
+        })
+
+    });
+
+
 }
 
 
@@ -232,6 +281,91 @@ $(document).ready(function () {
         loadResultspage();
 
     }
+
+        else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=1"){
+            loadDetailsPage();
+
+        }
+        else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=2"){
+        loadDetailsPage();
+
+        }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=3"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=4"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=5"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=6"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=7"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=8"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=9"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=10"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=11"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=12"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=13"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=14"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=15"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=16"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=17"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=18"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=19"){
+        loadDetailsPage();
+
+    }
+    else if(window.location.href == "http://sightfinder.fangwasan.com/details.php?halandid=20"){
+        loadDetailsPage();
+
+    }
+
+
+
+
 
 });
 
